a74e1c387a054a36bf6af7349ef4f96ad7da6149
zhangaaron <aaron.zhang@berkeley.edu>
2015-03-13 19:43:35 -0700

    added rahul's code for model convert to html

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -4,4 +4,9 @@ class Problem < ActiveRecord::Base
   has_and_belongs_to_many :instructors
   has_and_belongs_to_many :collections
   
+  def html5
+    text = "question '' do + \n" + self.text + '\n end'
+    %x(echo text > text.rb) #writes this to text.rb file since ruql prefers files
+    %x(ruql text.rb Html5 --template=preview.html.erb)
+  end
 end
