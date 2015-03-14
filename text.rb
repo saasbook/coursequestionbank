831596819b44ef1ec774427e03b56c782f03721b
Iryna Shustava <ishustava@berkeley.edu>
2015-03-13 22:55:12 -0700

    merged with master

--- text.rb
+++ text.rb
@@ -1,9 +1,9 @@
 quiz '' do 
  select_multiple :randomize => true do
-        text  'Which tiers in the three-tier architecture are involved in handling views?'
-        answer 'Presentation'
-        answer 'Logic'
-        distractor 'Persistence'
-        distractor 'Database'
-    end 
+               text  'Which tiers in the three-tier architecture are involved in handling views?'
+               answer 'Presentation'
+               answer 'Logic'
+               distractor 'Persistence'
+               distractor 'Database'
+               end 
  end
