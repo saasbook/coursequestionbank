0c3952f2fb3d8d7999502ccd9383b573ac4a9b17
zhangaaron <aaron.zhang@berkeley.edu>
2015-04-01 14:18:04 -0700

    fixed security issue with dashboard allowing all instructors to be viewed

--- app/controllers/instructor_controller.rb
+++ app/controllers/instructor_controller.rb
@@ -1,9 +1,9 @@
-class InstructorController < ApplicationController	
+class InstructorController < ApplicationController
 	def new 
 		render(:file => File.join(Rails.root, 'app/views/403.html'), :status => 403, :layout => false)
 	end
 
 	def show
-		@instructor = Instructor.find(params[:id]) 
+		@instructor = Instructor.find_by_id(session[:user_id])
 	end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Saasquizes::Application.routes.draw do
   get 'login' => 'sessions#login', :as => 'login'
   
   resources :problems
-  resources :instructor
+  get 'profile' => 'instructor#show', :as => 'profile'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
