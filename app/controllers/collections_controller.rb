4cc88089ea9c94031edb62e1fa5bee924c7773fe
zhangaaron <aaron.zhang@berkeley.edu>
2015-04-02 17:53:19 -0700

    wip

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,9 @@
+class CollectionsController < ApplicationController
+  def new
+    puts 'NEW CALLED'
+  end
+  def edit
+    puts 'EDIT CALLED'
+  end 
+
+end
---
+++ text.rb
@@ -0,0 +1,9 @@
+quiz '' do 
+ select_multiple :randomize => true do
+               text  'Which tiers in the three-tier architecture are involved in handling views?'
+               answer 'Presentation'
+               answer 'Logic'
+               distractor 'Persistence'
+               distractor 'Database'
+               end 
+ end
