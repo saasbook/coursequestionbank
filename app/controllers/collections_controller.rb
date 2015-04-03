cc1b99406b36eed6d1e94ed526e22680d5d83ede
zhangaaron <aaron.zhang@berkeley.edu>
2015-04-03 11:00:49 -0700

    need to fix database migration for instructor->collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,9 +1,19 @@
 class CollectionsController < ApplicationController
   def new
     puts 'NEW CALLED'
+    @collection = Collection.new
+
   end
   def edit
-    puts 'EDIT CALLED'
-  end 
+    @collection = Collection.find(params[:id])
+  end
+
+  def create
+    @collection = Collection.new params[:collection]
+    @collection.instructor = @current_user
+    @collection.created_at = DateTime.now
+    @collection.save
+    puts 'collection created successfuly and entered into DB'
+  end
 
 end
