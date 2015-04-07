1fc632cb84bfbd3a0254ff8e5d07c7932b3c4309
zhangaaron <aaron.zhang@berkeley.edu>
2015-04-07 02:28:47 -0700

    oops forgot to commit4

--- text.rb
+++ text.rb
@@ -1,8 +1,10 @@
 quiz '' do 
- choice_answer do
-    text "Which rendering system - Haml or Erb - allows not only Ruby variables but arbitrary Ruby code to be interpolated into a template?"
-    distractor 'Haml'
-    distractor 'Erb'
-    answer 'both'
-  end 
+   choice_answer :randomize => true do
+    text "An HTTP request must consist of both a(n) ----- and a(n) -----"
+    distractor "CRUD action, database"
+    distractor "header, cookie", :explanation => 'A header is part of the request, but a cookie is not necessarily included unless the server previously specified one.'
+    distractor "URL, wildcard", :explanation => 'A URL is necessary, but not a wildcard.'
+    answer "URI, HTTP request method", :explanation => "The URI alone isn't enough--the same URI used with two different methods can cause two different actions."
+  end
+ 
  end
