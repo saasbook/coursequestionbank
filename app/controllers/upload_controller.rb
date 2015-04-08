b08ac9721cc9dc3d9eff90f1719d5f2802ac07fa
zhangaaron <aaron.zhang@berkeley.edu>
2015-04-08 14:14:47 -0700

    added problem_type field to migration and problem.rb

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -8,10 +8,4 @@ class UploadController < ApplicationController
     flash[:notice] = "Sucessfully uploaded file"
     redirect_to problems_path 
   end
-  # def upload
-  #   uploaded_io = params[:ruql_file]
-  #   File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-  #     file.write(uploaded_io.read)
-  #   end
-  # end
 end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,5 +1,5 @@
 class Problem < ActiveRecord::Base
-  attr_accessible :created_date, :is_public, :last_used, :text
+  attr_accessible :created_date, :is_public, :last_used, :text, :problem_type
   has_and_belongs_to_many :tags
   belongs_to :instructor
   has_and_belongs_to_many :collections
