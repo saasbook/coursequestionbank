46f3a2b0de8f400b0cf3d025bcc1f6f6490b1f5c
zhangaaron <aaron.zhang@berkeley.edu>
2015-04-10 01:52:29 -0700

    slimming controllers, adding simple sanity checks against stupid users

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,33 +1,16 @@
 class CollectionsController < ApplicationController
   def new
-    puts 'NEW CALLED'
     @collection = Collection.new
-
   end
+
   def edit
     @collection = Collection.find(params[:id])
     @problems = @collection.problems
   end
+
   # creates a new collection with user specified values and sets as current collection
   def create
-    @collection = Collection.new params[:collection]
-    @collection.instructor =  @current_user
-    @collection.created_at = DateTime.now
-    @collection.save
-    @current_user.current_collection = @collection.id
-
-
-    puts 'COLLECTION IS ', @current_user.current_collection
-    @current_user.save
-    
-    puts 'collection created successfuly and entered into DB'
+    Collection.create_collection(params[:collection], @current_user)
     redirect_to profile_path
   end
-
-
-
-  # def swagger
-  #   do work
-  #   render json: {success: 'true'}
-
 end
