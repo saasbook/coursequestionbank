a4644c267b05379a35d83b62341599120906521c
zhangaaron <aaron.zhang@berkeley.edu>
2015-04-13 20:17:43 -0700

    refactoring the collections for model validations

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -10,12 +10,20 @@ class CollectionsController < ApplicationController
 
   # creates a new collection with user specified values and sets as current collection
   def create
-    Collection.create_collection(params[:collection], @current_user)
+    collection_hash = params[:collection]
+    if not (collection = @current_user.collections.create(collection_hash)).valid?
+      flash[:notice] =  collection.errors.join ' ,'
+      flash.keep
+    end
     redirect_to profile_path
   end
 
   def update
-    Collection.find(params[:id]).update_attributes(params[:collection])
+    if not (collection = Collection.update(params[:id], params[:collection])).valid?
+      flash[:notice] =  collection.errors.join ' ,'
+      flash.keep
+    end
     redirect_to profile_path
   end
+
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,6 @@
 class Collection < ActiveRecord::Base
   attr_accessible :last_used, :name
+  validates :name, presence: true, uniqueness: true
   has_and_belongs_to_many :problems
   belongs_to :instructor
   scope :collection, ->(collection_name) { where(name: collection_name) }
