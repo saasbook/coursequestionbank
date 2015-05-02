6418e2b38f781ac7b31986d2052f6d76a9fc29fb
zhangaaron <aaron.zhang@berkeley.edu>
2015-05-01 19:54:27 -0700

    rebase from master add seeds.rb to have priviledge

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,12 +1,10 @@
-
-
 Given /^I am signed in with provider "([^\"]*)"$/ do |provider| 
 	visit login_path
 	visit "/auth/#{provider.downcase}"
 end
 
 Given /^I am signed in with uid "([^\"]*)" and provider "([^\"]*)"$/ do |uid, provider| 
-	@instructor = Instructor.find_by_username(uid)
+	@instructor = Instructor.find_by_uid(uid)
 	visit login_path
 	Instructor.stub(:find_by_provider_and_uid).and_return(@instructor)
 	Instructor.stub(:create_with_omniauth).and_return(@instructor)
@@ -20,4 +18,3 @@ end
 Given /login was unsuccessful/ do
 	visit auth_failure_path
 end
-
