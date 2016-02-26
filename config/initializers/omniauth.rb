1bdebe478ce6d1adcd00da3a9e3eca26e2aaee26
jonsheu <bitbucket@jsheu.com>
2016-02-26 07:06:50 +0000

    Try to deploy to Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	if Rails.env.development? or Rails.env.test?
-  		provider :github, '1697958983c6320f708f', 'efa6306331ad08250cf380d9f25a84336f9b34cd'
+  		provider :github, '918cb574e68f5b18bc23', '0238b8b0717604a52fec7bef153c90c21ab673cb'
   	else
-  		provider :github, '8cbb590d480c4dd80442', '8a4c2ff770cecf63a51b1cfb9db9a00709c0045c'
+  		provider :github, '918cb574e68f5b18bc23', '0238b8b0717604a52fec7bef153c90c21ab673cb'
   	end
 end
