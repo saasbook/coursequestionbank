211faed77a0bfc950f1fbf47189ece50080c8fe9
jshoe <j.sheu@berkeley.edu>
2016-03-01 16:12:49 -0800

    Add OAuth for Heroku callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	if Rails.env.development? or Rails.env.test?
   		provider :github, '918cb574e68f5b18bc23', '0238b8b0717604a52fec7bef153c90c21ab673cb'
-  	else
+	elsif Rails.env.production?
+			provider :github, 'c6ca221ca8c58e5c8ea3', '6f1cc725cc56cd73e2c1f65e36e449b7419807e9'
+  else
   		provider :github, '918cb574e68f5b18bc23', '0238b8b0717604a52fec7bef153c90c21ab673cb'
   	end
 end
