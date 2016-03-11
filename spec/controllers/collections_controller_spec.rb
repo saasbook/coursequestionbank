5d7e8bbf9e7c90a2bb772a835168c8e5cc8c864e
brandonpickering <brandonpickering95@gmail.com>
2016-03-11 21:45:59 +0000

    Write collections controller tests

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe CollectionsController do
+  
+  describe 'update_multiple_tags' do
+    
+    it 'should add tags' do
+      problem1 = Problem.create.id
+      problem2 = Problem.create.id
+      checked_hash = {problem1.to_s => "1", problem2.to_s => "1"}
+      post :update_multiple_tags, :tag_names => "tag1, tag2", :checked_problems => checked_hash
+      expect(Problem.find(problem1).tags.size).to eq(2)
+      expect(Problem.find(problem2).tags.size).to eq(2)
+    end
+  end
 end
