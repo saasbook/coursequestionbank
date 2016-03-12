5afab40784a815e3ea0115d5d662ca4f648ef354
brandonpickering <brandonpickering95@gmail.com>
2016-03-12 01:25:50 +0000

    Write Collection#add_problem test

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -6,4 +6,13 @@ describe Collection do
     expect { Collection.new.save! }.to raise_error(ActiveRecord::RecordInvalid)
     
   end
+  
+  describe 'add_problem' do
+    it 'should add a problem to the collection' do
+      collection = Collection.new
+      problem = Problem.new
+      collection.add_problem(problem)
+      expect(collection.problems).to include(problem)
+    end
+  end
 end
