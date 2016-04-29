c1cd855b4daf97cdaec472cb834779fa1caec922
Jesmin Ngo <xjesmin.ngo@gmail.com>
2016-04-29 09:09:06 +0000

    exporting in new uid format

--- lib/ruql_renderer.rb
+++ lib/ruql_renderer.rb
@@ -8,6 +8,9 @@ class RuqlRenderer
     answers = json_hash["answers"]
     return ruql_true_false(json_hash) if json_hash["question_type"] == "TrueFalse"
     result << ruql_question_header(json_hash, uuid)
+    if not uuid.blank?
+      result << "\n  uid " + uuid
+    end
     result << "\n  text " + json_hash["question_text"].inspect
     answers.each do |answer| # answers first
       result << ruql_answer_line(answer) if answer["correct"]
@@ -34,18 +37,13 @@ class RuqlRenderer
   end
 
   def self.ruql_question_header(json_hash, uuid)
-    if uuid && uuid != '-1'
-      uuid_s = " :question_uuid => '" + uuid.to_s + "'"
-    else
-      uuid_s = ""
-    end
     line = case json_hash["question_type"]
-      when "SelectMultiple" then "select_multiple" + uuid_s
-      when "MultipleChoice" then "choice_answer" + uuid_s
-      when "FillIn" then "fill_in" + uuid_s
+      when "SelectMultiple" then "select_multiple"
+      when "MultipleChoice" then "choice_answer"
+      when "FillIn" then "fill_in"
       else ""
     end
-    options = ['randomize', 'raw', 'question_uuid'].select{|x| json_hash[x]}.map{|x| ":#{x} => true"}.join(', ')
+    options = ['randomize', 'raw'].select{|x| json_hash[x]}.map{|x| ":#{x} => true"}.join(', ')
     line += ' ' + options if options != ''
     return line + " do"
   end
