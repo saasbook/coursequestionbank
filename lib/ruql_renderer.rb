4467f3696147e4e29c595c67a040077dc07b1c49
kimya <kimyakhoshnan@berkeley.edu>
2016-04-29 07:14:38 +0000

    added new and old uuids as attributes to the ruql source

--- lib/ruql_renderer.rb
+++ lib/ruql_renderer.rb
@@ -1,13 +1,15 @@
 
 class RuqlRenderer
 
-  def self.render_from_json(json_code, uuid)
+  def self.render_from_json(json_code, uuid, prev_uuid)
     result = ""
     return "" if json_code == nil || json_code.length <= 2
     json_hash = JSON.parse(json_code)
     answers = json_hash["answers"]
     return ruql_true_false(json_hash) if json_hash["question_type"] == "TrueFalse"
-    result << ruql_question_header(json_hash, uuid)
+    result << ruql_question_header(json_hash)
+    result << "\n  uuid #{uuid}" if uuid
+    result << "\n  # uuid #{prev_uuid}" if prev_uuid
     result << "\n  text " + json_hash["question_text"].inspect
     answers.each do |answer| # answers first
       result << ruql_answer_line(answer) if answer["correct"]
@@ -33,16 +35,11 @@ class RuqlRenderer
     return line
   end
 
-  def self.ruql_question_header(json_hash, uuid)
-    if uuid && uuid != '-1'
-      uuid_s = " :question_uuid => '" + uuid.to_s + "'"
-    else
-      uuid_s = ""
-    end
+  def self.ruql_question_header(json_hash)
     line = case json_hash["question_type"]
-      when "SelectMultiple" then "select_multiple" + uuid_s
-      when "MultipleChoice" then "choice_answer" + uuid_s
-      when "FillIn" then "fill_in" + uuid_s
+      when "SelectMultiple" then "select_multiple"
+      when "MultipleChoice" then "choice_answer"
+      when "FillIn" then "fill_in"
       else ""
     end
     options = ['randomize', 'raw'].select{|x| json_hash[x]}.map{|x| ":#{x} => true"}.join(', ')
