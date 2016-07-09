aa8cb81491d35fc6c5c99040a93e72ad432a82f6
Armando Fox <fox@cs.berkeley.edu>
2016-07-09 12:35:21 -0700

    Figaro-ized the app and generated new Github OAuth keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production?
-    provider :github, 'c6ca221ca8c58e5c8ea3', '6f1cc725cc56cd73e2c1f65e36e449b7419807e9' # Heroku keys
-  elsif Rails.env.development? or Rails.env.test?
-    provider :developer, :fields => [:uid], :uid_field => [:uid]         # see seeds.rb for available :developer logins 
+  provider :github, Figaro.env.github_key!, Figaro.env.github_secret!
+  if Rails.env.development? or Rails.env.test?
+    provider :developer, :fields => [:uid], :uid_field => [:uid]
+    # see seeds.rb for available :developer logins 
     SessionController.skip_before_filter :verify_authenticity_token, only: :create # to avoid problems when testing with SSL turned on with OmniAuth Developer strategy
-
-    provider :github, '9a486077b6009822c7fc', '1b1a6c928849e18152ba49c4011aac7de2d10b7c' # localhost:3000
-  else                          # ?? when would this happen??
-    provider :github, '918cb574e68f5b18bc23', '0238b8b0717604a52fec7bef153c90c21ab673cb'
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Coursequestionbank::Application.config.secret_token = 'd05a3e4a812c8aaf794ebb8cf793a547fbd8512859406ff304701668092eb00fefe0901c8e09d009f12a35e760a9de7cc1834de2da5043d0cdb52e21c6897f4f'
+Coursequestionbank::Application.config.secret_token = Figaro.env.app_secret!
+
