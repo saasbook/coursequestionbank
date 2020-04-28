963cc4163e25f9bbeabf79cb2d94805629c3abd7
Armando Fox <fox@cs.berkeley.edu>
2020-04-28 12:37:55 -0700

    close CVE-2018-3760 by disabling lazy asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
 Coursequestionbank::Application.configure do
-    config.active_support.deprecation = :notify
+  config.active_support.deprecation = :notify
+  config.assets.compile = false # for production/Heroku, and to close serious CVE-2018-3760
 end
